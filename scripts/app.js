9088cfb3c74170239ac5c1e1f989e7bff470a9bb
ElianaArias <ariaselianamariel@gmail.com>
2023-11-15 18:45:41 -0300

    sigo probando con buttonup

--- scripts/app.js
+++ scripts/app.js
@@ -1,26 +1,35 @@
-const arriba = document.getElementById("arriba")
+// Botón para Subir Arriba
+// -------------------------------
 
+
+// Busca un elemento con el ID "arriba" y lo almacena en la variable "arriba", quiere decir que lo guarda en la memoria de la compu.
+const arriba = document.getElementById("arriba")
+// Agrega un "escuchador de eventos" que espera a que hagas clic en el botón.
 arriba.addEventListener("click", () => {
-    
-    window.scrollTo(0, 0);
+    // Hace que la ventana (nuestra pág) se desplace a la posición vertical 0, nos lleva al inicio de la página.
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
 });
 
 
-var buttonUp = document.getElementById("arriba");
+// Busca nuevamente el elemento con el ID "arriba" y lo almacena en la variable "buttonUp".
+const buttonUp = document.getElementById("arriba");
 
 
-
+// Agrega una función que se ejecuta cada vez que ocurre un desplazamiento (scroll) en la página.
 window.onscroll = function() {
-
+    // Obtiene la cantidad de desplazamiento vertical de la página y lo almacena en la variable "scroll".
     var scroll = document.documentElement.scrollTop;
 
 
-    
+    // Si el desplazamiento es mayor a 500 píxeles...
     if (scroll > 500) {
-        
+        // Cambia el tamaño (escala) del botón para que haga el efecto de aparecer al llegar a los 500px.
         buttonUp.style.transform = "scale(1)";
     } else if (scroll < 500) {
-        
+        // Si el desplazamiento es menor a 500 píxeles, cambia el tamaño del botón para hacerlo pequeño e invisible.
         buttonUp.style.transform = "scale(0)";
     }
 }
